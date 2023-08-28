a5d7ac69c66d2a880eff094558bf73179acd290a
MInesBarbosaa <93823221+MInesBarbosaa@users.noreply.github.com>
2023-08-28 11:04:31 +0100

    Forum CSS and JS

--- js/forum.js
+++ js/forum.js
@@ -1,22 +1,42 @@
-// the form of the comment section
+// Form Elements
 const form_comment = document.getElementById("form_comment");
-const comment_button = document.getElementById("comment_button");
-const userComment = document.getElementById("userComment");
-
-// the form of the contact section
 const form_contact = document.getElementById("form_contact");
-//...
+const userComment = document.getElementById("userComment");
+const name = document.getElementById("name"); // Add this line
+const email = document.getElementById("email"); // Add this line
+const message = document.getElementById("message"); // Add this line
 
-comment_button.addEventListener("click", function (e) {
-    e.preventDefault();
-    alert(" Your comment is submitted. Thank you!");
-    // if nothing is written in the comment section, user shouldn't be able to submit it.
-    // after comment is submitted, clear what's written in the comment section.
-    //similar functionality should be added to contact-form 
+// Comment Button Event Listener
+form_comment.addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+  const commentValue = userComment.value.trim();
+  
+  if (commentValue !== "") {
+    const newComment = document.createElement("div");
+    newComment.className = "comment";
+    newComment.innerHTML = `
+      <img src="../imgs/user.png" alt="User 2">
+      <p>${commentValue}</p>
+    `;
+
+    const commentsContainer = document.querySelector(".comments-section");
+    commentsContainer.appendChild(newComment);
+    
+    userComment.value = ""; // Clear the input field
+    
+    alert("Thank you for your comment! You can find more comments above.");
+  }
 });
 
-const comment = userComment.addEventListener("keypress", function (e) {
-  const catchComment = e.target.value;
-  console.log(catchComment);
-  console.log(catchComment.length);
+// Contact Form Submit Event Listener
+form_contact.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  name.value = "";
+  email.value = "";
+  message.value = "";
+  
+  alert("Thank you for your message! We will answer you as soon as possible.");
 });
+
